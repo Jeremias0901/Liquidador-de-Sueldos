976d8eea6ed0aab51cf21dd43cdb73e541360d4d
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-10-09 21:11:46 -0300

    rename

--- Aguinaldo/js/index.js
+++ Aguinaldo/js/index.js
@@ -1,22 +1,27 @@
-var PSem = [];
-var SSem = [];
+const meses = [
+  ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO',],
+  ['JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE']
+];
+
+let sueldos = [];
 var MAYPS = 0;
 var MAYSS = 0;
 var i;
 var VAL = "";
-function Cargar(SEMESTRE) {
-	PSem[0] = parseInt(document.getElementById("ENERO").value);
-	PSem[1] = parseInt(document.getElementById("FEBRERO").value);
-	PSem[2] = parseInt(document.getElementById("MARZO").value);
-	PSem[3] = parseInt(document.getElementById("ABRIL").value);
-	PSem[4] = parseInt(document.getElementById("MAYO").value);
-	PSem[5] = parseInt(document.getElementById("JUNIO").value);
-	SSem[0] = parseInt(document.getElementById("JULIO").value);
-	SSem[1] = parseInt(document.getElementById("AGOSTO").value);
-	SSem[2] = parseInt(document.getElementById("SEPTIEMBRE").value);
-	SSem[3] = parseInt(document.getElementById("OCTUBRE").value);
-	SSem[4] = parseInt(document.getElementById("NOVIEMBRE").value);
-	SSem[5] = parseInt(document.getElementById("DICIEMBRE").value);
+
+function cargar() {
+	sueldos[0] = parseInt(document.getElementById(meses[0][0]).value);
+	PSem[1] = parseInt(document.getElementById(meses[0][1]).value);
+	PSem[2] = parseInt(document.getElementById(meses[0][2]).value);
+	PSem[3] = parseInt(document.getElementById(meses[0][3]).value);
+	PSem[4] = parseInt(document.getElementById(meses[0][4]).value);
+	PSem[5] = parseInt(document.getElementById(meses[0][5]).value);
+	SSem[0] = parseInt(document.getElementById(meses[1][0]).value);
+	SSem[1] = parseInt(document.getElementById(meses[1][1]).value);
+	SSem[2] = parseInt(document.getElementById(meses[1][2]).value);
+	SSem[3] = parseInt(document.getElementById(meses[1][3]).value);
+	SSem[4] = parseInt(document.getElementById(meses[1][4]).value);
+	SSem[5] = parseInt(document.getElementById(meses[1][5]).value);
 	VAL = "true";
 	for (i = 0; (i < 6) && (VAL == "true"); i++) {
 		VAL = "false";
@@ -50,27 +55,21 @@ function Calcular() {
 	}
 	MAYPS = MAYPS / 2;//Aguialdo Primer Semestre
 	MAYSS = MAYSS / 2;//Aguialdo Segundo Semestre
-	document.getElementById("AGUINALDO_1").value = MAYPS; //Aguialdo Primer Semestre
+	document.getElementById("resultado__value").value = MAYPS; //Aguialdo Primer Semestre
 	document.getElementById("AGUINALDO_2").value = MAYSS; //Aguialdo Segundo Semestre
 	document.getElementById("AGUINALDO_MAS_MES_1").value = PSem[5] + MAYPS; //Total en junio
 	document.getElementById("AGUINALDO_MAS_MES_2").value = SSem[5] + MAYSS; //Total en diciembre}
 	function Desplegar(SEMESTRE, IN_OUT) {
-		/*
-	SEMESTRE = {1, 2};
-	IN_OUT = {
-		0 => INPUT
-	1 => OUTPUT };
-	*/
 		if (IN_OUT == 1) {
 			if (SEMESTRE == 1) {
 				/*DESACTIVAR OUTPUTS SEMESTRE 2*/
 				document.getElementById('ID_OUTPUT_AGUINALDO_2').style.display = "none";
 				/*ACTIVAR OUTPUTS SEMESTRE 1*/
-				document.getElementById('ID_OUTPUT_AGUINALDO_1').style.display = "block";
+				document.getElementById('ID_OUTPUT_resultado__value').style.display = "block";
 			}
 			if (SEMESTRE == 2) {
 				/*DESACTIVAR OUTPUTS SEMESTRE 1*/
-				document.getElementById('ID_OUTPUT_AGUINALDO_1').style.display = "none";
+				document.getElementById('ID_OUTPUT_resultado__value').style.display = "none";
 				/*ACTIVAR OUTPUTS SEMESTRE 2*/
 				document.getElementById('ID_OUTPUT_AGUINALDO_2').style.display = "block";
 			}
@@ -94,7 +93,7 @@ function Calcular() {
 			}
 			if (SEMESTRE == 2) {
 				/*DESACTIVAR OUTPUTS SEMESTRE 1*/
-				document.getElementById('ID_INPUT_AGUINALDO_1').style.display = "none";
+				document.getElementById('ID_INPUT_resultado__value').style.display = "none";
 				/*ACTIVAR OUTPUTS SEMESTRE 2*/
 				document.getElementById('ID_INPUT_AGUINALDO_2').style.display = "block";
 			}
@@ -102,7 +101,7 @@ function Calcular() {
 	}
 	function Activar_Input_2do_Semestre() {
 		/*DESACTIVAR OUTPUTS SEMESTRE 1*/
-		document.getElementById('ID_OUTPUT_AGUINALDO_1').style.display = "none";
+		document.getElementById('ID_OUTPUT_resultado__value').style.display = "none";
 		/*DESACTIVAR BOTON 1 SEMESTRE*/
 		document.getElementById('BOTON_1ER_SEM').className = "INPUT_BOTON";
 		/*ACTIVAR BOTON 2 SEMESTRE*/
@@ -117,3 +116,8 @@ function Calcular() {
 		document.getElementById('ID_BOTON_2').style.display = "block";
 	}
 }
+/*
+function main() {
+  {sueldos} = cargar()
+}
+ */
