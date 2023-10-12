ed9fc178d7ec21c223fcc16e8edf48972bdb7e49
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-10-11 22:44:18 -0300

    fix - omision de carteles

--- view/resources/loadComponents.js
+++ view/resources/loadComponents.js
@@ -1,7 +1,15 @@
 const includeComp = ({path, tag}) => {
   fetch(path)
     .then(res => res.text())
-    .then(res => document.querySelector(tag).innerHTML += res);
+    .then(res => {
+      try {
+        document.querySelector(tag).innerHTML += res;
+      } catch (err) {
+        if(!/(sign)/.test(tag)){ // omitiendo errores de carga de carteles
+          console.error(`No se encontro el elemento ${tag}`);
+        }
+      }
+    })
 }
 
 fetch('/view/resources/components.json')
